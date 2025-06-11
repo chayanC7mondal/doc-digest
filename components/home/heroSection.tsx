a958f2d104d7a2d6a6ea3f1a55a8d8e01bbea92b
chayanc7mondal <mitalichayan18082004@gmail.com>
2025-06-11 18:43:20 +0530

    feat: implement Badge component, update button styles, and refine Tailwind CSS configuration

--- components/home/heroSection.tsx
+++ components/home/heroSection.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { Sparkles } from "lucide-react";
+import { Badge } from "../ui/badge";
 
 export default function HeroSection() {
   return (
     <section>
       <div className="">
         <div className="flex">
-          <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse" />
-          <p>Powered by AI</p>
+          <Badge>
+            <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse" />
+            <p>Powered by AI</p>
+          </Badge>
         </div>
         <h1>Transform PDFs into concise summaries</h1>
         <h2>Get a beautiful summary reel of the document in seconds.</h2>

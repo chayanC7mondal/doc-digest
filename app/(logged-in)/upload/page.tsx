55b8d77ec0d53536e2abac3ca16597e1c300aea8
chayanc7mondal <mitalichayan18082004@gmail.com>
2025-06-13 12:06:00 +0530

    feat: enhance upload page with structured layout and file upload functionality

--- app/(logged-in)/upload/page.tsx
+++ app/(logged-in)/upload/page.tsx
@@ -7,8 +7,10 @@ export default function page() {
     <section className="min-h-screen">
       <BgGradient />
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
-        <UploadHeader />
-        <UploadForm />
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <UploadHeader />
+          <UploadForm />
+        </div>
       </div>
     </section>
   );
---
+++ components/upload/uploadFormInput.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+interface UploadFormInputProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export default function UploadFormInput({ onSubmit }: UploadFormInputProps) {
+  return (
+    <div>
+      <form className="flex flex-col gap-6" onSubmit={onSubmit}>
+        <div className="flex justify-end items-center gap-1.5">
+          <Input
+            type="file"
+            id="file"
+            name="file"
+            accept="application/pdf"
+            className=""
+          />
+          <Button className="bg-rose-500">Upload your PDF</Button>
+        </div>
+      </form>
+    </div>
+  );
+}

6e00da8b44eb1cddd39512e37627b673756db03b
chayanc7mondal <mitalichayan18082004@gmail.com>
2025-06-25 23:07:20 +0530

    feat: implement DeleteButton component with confirmation dialog for summary deletion

--- components/summaries/delete-button.tsx
+++ components/summaries/delete-button.tsx
@@ -1,15 +1,69 @@
+"use client";
+
 import { Trash2 } from "lucide-react";
 import React from "react";
 import { Button } from "../ui/button";
 
-export default function DeleteButton() {
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+
+interface DeleteButtonProps {
+  summaryId: string;
+}
+
+export default function DeleteButton({ summaryId }: DeleteButtonProps) {
+  const [open, setOpen] = useState(false);
+
+  const handleDelete = async () => {
+    // Implement the delete logic here
+    //await deleteSummary(summaryId);
+    setOpen(false);
+  };
+
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      className="text-gray-700 bg-gray-50 border border-gray-200 hover:text-rose-600 hover:bg-rose-50 "
-    >
-      <Trash2 className="w-4 h-4" />
-    </Button>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="text-gray-700 bg-gray-50 border border-gray-200 hover:text-rose-600 hover:bg-rose-50 "
+        >
+          <Trash2 className="w-4 h-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Delete Summary</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete this summary? This action cannot be
+            undone.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button
+            variant="ghost"
+            className=" px-2 bg-gray-50 border border-gray-200 hover:text-gray-700 hover:bg-gray-200 hover:border-gray-700 "
+            onClick={() => setOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="destructive"
+            className="px-2 text-gray-700 bg-gray-50 border border-gray-200 hover:text-rose-600 hover:bg-rose-50 hover:border-rose-700 "
+            onClick={handleDelete}
+          >
+            Delete
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }

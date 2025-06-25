c4a9a09a6e1d37474ac469d73da879350ef519f0
chayanc7mondal <mitalichayan18082004@gmail.com>
2025-06-26 01:11:05 +0530

    style: enhance Upload a PDF link with consistent styling

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -42,7 +42,12 @@ const Header = () => {
       <div className="flex lg:justify-end lg:flex-1">
         <SignedIn>
           <div className="flex gap-2 items-center">
-            <NavLink href="/upload">Upload a PDF</NavLink>
+            <NavLink
+              href="/upload"
+              className="text-gray-700 hover:text-rose-500 transition-all duration-300 font-medium relative after:absolute after:bottom-[-4px] after:left-0 after:h-[2px] after:w-0 hover:after:w-full after:bg-rose-400 after:transition-all after:duration-300"
+            >
+              Upload a PDF
+            </NavLink>
             <div className="text-white">Pro</div>
             <SignedIn>
               <UserButton />
@@ -64,43 +69,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// import React from "react";
-// import { FileText } from "lucide-react";
-// import { Button } from "../ui/button";
-// import NavLink from "./navlink";
-// const Header = () => {
-//   const isLoggedIn = false;
-//   return (
-//     <nav className="container flex item-center justify-between py-4 lg:px-8 px-2">
-//       <div className="flex lg:flex-1">
-//         <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
-//           <FileText className="w-5 h-5 lg:w-8 lg:h-8 textigray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
-//           <span className="font-extrabold lg:text-xl text-gray-900">
-//             DocDigest
-//           </span>
-//         </NavLink>
-//       </div>
-
-//       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
-//         <NavLink href="/#pricing">Pricing</NavLink>
-//         {isLoggedIn && <NavLink href="/dashboard">Your Summaries</NavLink>}
-//       </div>
-//       <div className="flex lg:justify-end lg:flex-1">
-//         {isLoggedIn ? (
-//           <div className="flex gap-2 items-center">
-//             <NavLink href="/upload">Upload a PDF</NavLink>
-//             <div>Pro</div>
-//             <Button>User</Button>
-//           </div>
-//         ) : (
-//           <div>
-//             <NavLink href="/sign-in">Sign In</NavLink>
-//           </div>
-//         )}
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Header;

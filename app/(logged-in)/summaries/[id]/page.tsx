fb76891c6046488c5365f19d480d4ac6edbe51bc
chayanc7mondal <mitalichayan18082004@gmail.com>
2025-06-26 13:51:28 +0530

    feat: implement SummaryPage component with summary fetching and error handling

--- app/(logged-in)/summaries/[id]/page.tsx
+++ app/(logged-in)/summaries/[id]/page.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import BgGradient from "@/components/common/bg-gradient";
+import { getSummaryById } from "@/lib/summaries";
+import { notFound } from "next/navigation";
 
 export default async function SummaryPage(props: {
   params: Promise<{ id: string }>;
 }) {
   const params = await props.params;
   const id = params.id;
-  return <div>SummaryPage {id}</div>;
+  const summary = await getSummaryById(id);
+  // if (!summary) {
+  //   notFound();
+  // }
+  return (
+    <div className="relative isolate min-h-screen bg-linear-to-b from-rose-50/40 to-white ">
+      <BgGradient />
+    </div>
+  );
 }

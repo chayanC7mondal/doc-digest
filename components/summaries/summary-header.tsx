eb6d2dc1de917e912d6483e22dbc679d38f8ba97
chayanc7mondal <mitalichayan18082004@gmail.com>
2025-06-26 20:32:50 +0530

    feat: enhance SourceInfo and SummaryHeader components with additional props and styling

--- components/summaries/summary-header.tsx
+++ components/summaries/summary-header.tsx
@@ -16,7 +16,7 @@ function SummaryHeader({
   return (
     <div className="flex gap-4 mb-4 justify-between">
       <div className="sapace-y-6">
-        <div className="flex flex-wrap items-center gap-4">
+        <div className="flex flex-wrap items-center gap-4 pb-7">
           <Badge
             variant="secondary"
             className="relative px-4 py-1.5 text-sm font-medium bg-white/80 backdrop-blur-xs rounded-full hover:bg-white/90 transition-all duration-200 shadow-xs hover:shadow-md"
@@ -37,8 +37,13 @@ function SummaryHeader({
             {readingTime} min read
           </div>
         </div>
+        <h1 className="text-2xl lg:text-4xl font-bold lg:tracking-tight">
+          <span className="bg-linear-to-r from-rose-600 to-orange-600 bg-clip-text text-transparent">
+            {title}
+          </span>
+        </h1>
       </div>
-      <div>
+      <div className="self-start">
         <Link href="/dashboard">
           <Button
             variant={"link"}
